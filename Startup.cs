7db0f2f130afec75bff38f33377c1d24f2641010
ChristianKern97 <ikerneii97@gmail.com>
2021-02-21 22:25:13 +0100

    Smaller fixes

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace forkgg
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
