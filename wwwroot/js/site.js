76fa79946d4f83abe3cbfbb7a4f936f7c2c2dfb6
ChristianKern97 <ikerneii97@gmail.com>
2021-02-23 16:21:38 +0100

    Updated doc sidebar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-setScreenshotDescriptionHeight();
+$(document).ready(function () {
+    setScreenshotDescriptionHeight();
+
+    $('.title').each((index, title) =>{
+        mut.observe(title, options);
+    });
+    $('.entry').each((index, entry) =>{
+        mut.observe(entry, options);
+    });
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            let hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top -60
+            }, 0, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                //window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
 $(window).on('resize', function(){
     setScreenshotDescriptionHeight();
 })
@@ -36,14 +69,12 @@ function setScreenshotDescriptionHeight(){
         if (this.clientHeight > maxHeight)
             maxHeight = this.clientHeight;
     });
-    console.log("Max height of descriptions is "+maxHeight);
     descDiv.height(maxHeight);
 }
 
 
 
 function LoadImages(){
-    console.log("loading images");
     $('img[lazysrc]').each( function(){
             //* set the img src from data-src
             $( this ).attr( 'src', $( this ).attr( 'lazysrc' ) );
@@ -56,4 +87,22 @@ document.addEventListener('readystatechange', event => {
         LoadImages();
     }
 });
+
+
+let options = {attributes: true, childList: true, subtree: true};
+let mut = new MutationObserver(function(mutations, mut){
+    for(let mutation of mutations){
+        if (mutation.type === "attributes" && mutation.attributeName === "class"){
+            if(mutation.target.getAttribute("class").includes("active")){
+                $('.chapter').each(function(){
+                   if($(this).hasClass('active'))
+                       $(this).removeClass('active');
+                });
+                
+                mutation.target.parentElement.setAttribute("class", "chapter active");
+            }
+        }
+    }
+});
+
 
