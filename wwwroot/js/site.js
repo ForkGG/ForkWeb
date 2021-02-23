610fe62b070fdcd1b439c60ffef8e2020849a209
ChristianKern97 <ikerneii97@gmail.com>
2021-02-23 16:51:26 +0100

    Improved scrolling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,36 +4,6 @@
 // Write your Javascript code.
 $(document).ready(function () {
     setScreenshotDescriptionHeight();
-
-    $('.title').each((index, title) =>{
-        mut.observe(title, options);
-    });
-    $('.entry').each((index, entry) =>{
-        mut.observe(entry, options);
-    });
-
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            let hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top -60
-            }, 0, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                //window.location.hash = hash;
-            });
-        } // End if
-    });
 });
 
 $(window).on('resize', function(){
@@ -89,20 +59,6 @@ document.addEventListener('readystatechange', event => {
 });
 
 
-let options = {attributes: true, childList: true, subtree: true};
-let mut = new MutationObserver(function(mutations, mut){
-    for(let mutation of mutations){
-        if (mutation.type === "attributes" && mutation.attributeName === "class"){
-            if(mutation.target.getAttribute("class").includes("active")){
-                $('.chapter').each(function(){
-                   if($(this).hasClass('active'))
-                       $(this).removeClass('active');
-                });
-                $(mutation.target.parentElement).addClass('active');
-                //mutation.target.parentElement.setAttribute("class", "chapter active");
-            }
-        }
-    }
-});
+
 
 
